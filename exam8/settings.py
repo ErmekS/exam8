e244a29a0e1fabaa49fc133a3023978d0f154a99
ErmekSolpuev <ermeksee27@gmail.com>
2022-08-27 16:26:05 +0600

    Написал CRUD для товаров:
    Список товаров (главная)
    Просмотр товара
    Создание товара
    Редактирование товара
    Удаление товара

--- exam8/settings.py
+++ exam8/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'exam8.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'webapp/migrations/../webapp/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- webapp/admin.py
+++ webapp/admin.py
@@ -5,11 +5,11 @@ from webapp.models import Product, Review
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'product_name', 'category', 'product_description', 'created_time', 'updated_time']
+    list_display = ['id', 'product_name', 'category', 'product_description', 'created_time', 'updated_time', 'product_img']
     list_display_links = ['product_description']
     list_filter = ['product_name', 'category']
     search_fields = ['category']
-    fields = ['product_name', 'product_description', 'category', 'created_time', 'updated_time']
+    fields = ['product_name', 'product_description', 'category', 'created_time', 'updated_time', 'product_img']
     readonly_fields = ['created_time', 'updated_time']
 
 
